bebd1021d7ef2e489cc9bf578e48ceecaa82bd1e
Hamanameia <hamanameia1@gmail.com>
2024-09-12 13:32:35 -0300

    Fim

---
+++ Baxkairas/AdalbertoEinstenio.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace Baxkairas
+{
+    public class AdalbertoEinstenio
+    {
+        public static double[] Baxkaira(double a, double b, double c)
+        {
+            var delta = b * b - 4 * a * c;
+            var x1 = (-b + Math.Sqrt(delta))/2*a;
+            var x2 = (-b - Math.Sqrt(delta)) / 2 * a;
+
+            if (delta < 0)
+            {
+
+                return null;
+            }
+
+            
+
+            return new double[] {x1,x2};
+        }
+    }
+}
---
+++ QuadroBranco/Canetao.cs
@@ -0,0 +1,16 @@
+using Baxkairas;
+    
+namespace QuadroBranco
+{
+    public class Canetao
+    {
+       
+            [Fact]
+            public void Baxkairaa()
+            {
+                Assert.Equal([3,2], AdalbertoEinstenio.Baxkaira(1, -5, 6));
+            }
+        
+        
+    }
+}
